ee09ea99dd6a81398714b8d706be9bc25aa445e2
ChiragKaushik21 <chiragka2002@gmail.com>
2024-07-26 13:27:03 +0530

    u

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,22 +7,34 @@ const HomePage = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
-    <h1 className="text-2xl font-bold mb-4">Bank Recruitment Registrations</h1>
-    <Link to="/register">
-      <button className="bg-blue-500 text-white px-4 py-2 rounded mb-4">Add New</button>
-    </Link>
-    <h1>Registered Users</h1>
-    <ul className="grid grid-cols-2 grid-rows-1 gap-20">
-      {registrations.map((reg) => (
-        <li key={reg.id} className="mb-2">
-          <Link to={`/user/${reg.id}`} className="text-blue-500 hover:underline">
-            {reg.name}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-  
+      <h1 className="text-2xl font-bold mb-4">
+        Bank Recruitment Registrations
+      </h1>
+      <Link to="/register">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded mb-4">
+          Add New
+        </button>
+      </Link>
+      <h1>Registered Users</h1>
+      <ul className="grid grid-cols-2 grid-rows-1 gap-20">
+        {registrations.length === 0 ? (
+          <li className="mb-2 text-center text-gray-500">
+           No user Registered! Register a new user.
+          </li>
+        ) : (
+          registrations.map((reg) => (
+            <li key={reg.id} className="mb-2">
+              <Link
+                to={`/user/${reg.id}`}
+                className="text-blue-500 hover:underline"
+              >
+                {reg.name}
+              </Link>
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
   );
 };
 
